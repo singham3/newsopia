5038235c51279b3b9633d8c136e1c62cfeb756c8
lokesh sahu <lsahu05@gmail.com>
2026-05-12 15:58:57 +0530

    made changes

--- app/api/news/route.ts
+++ app/api/news/route.ts
@@ -4,10 +4,12 @@ export const dynamic = 'force-dynamic';
 
 const fetchNewsData = async (category: string , page: number = 1) => {
     try {
-        const url = category ? `https://inshorts.com/api/en/search/trending_topics/${category}?page=${page}&type=NEWS_CATEGORY` : `https://inshorts.com/api/en/search/trending_topics`;
+        const currentPage = parseInt(page, 10) || 1;
+        const url = `https://inshorts.com/api/en/search/trending_topics/${category}?page=${currentPage}&type=NEWS_CATEGORY`;
         const response = await fetch(url);
         const data = await response.json();
-        const newPage = page + 1;
+        const newPage = currentPage + 1;
+        console.log(url);
         if (data.data.news_list) {
             const newsList = data.data.news_list.map((item: any) => ({
                 id: item.news_obj.hash_id,
@@ -28,16 +30,43 @@ const fetchNewsData = async (category: string , page: number = 1) => {
     }
 }
 
+const fetchTrendingTopics = async (offset: string) => {
+    try {
+        const url = `https://inshorts.com/api/en/news?category=top_stories&max_limit=240&include_card_data=true${offset == '1' ? '' : '&offset=' + offset}`;
+
+        const response = await fetch(url);
+        const data = await response.json();
+        const newOffset = data.data.min_news_id;
+        console.log(url);
+        if (data.data.news_list) {
+            const newsList = data.data.news_list.map((item: any) => ({
+                id: item.news_obj.hash_id,
+                title: item.news_obj.title,
+                description: item.news_obj.content,
+                author: item.news_obj.author_name,
+                date: formatTimestamp(item.news_obj.created_at),
+                category: item.news_obj.category_names,
+                image: item.news_obj.image_url,
+                url: item.news_obj.source_url,
+            }));
+            return { newsList, newPage: newOffset };
+        }
+        return { newsList: [], newPage: '' };
+    } catch (error) {
+        console.error('Error fetching trending topics:', error);
+        return { newsList: [], newPage: '' };
+    }
+}
 // Static news data to simulate API responses
 
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
     const pageParam = searchParams.get('page');
-    const page = pageParam ? parseInt(pageParam, 10) : 1;
-    const category = searchParams.get('category') || 'all';
+    const page = pageParam;
+    const category = searchParams.get('category');
 
-    const { newsList, newPage } = await fetchNewsData(category, page);
-    
+    const { newsList, newPage } = category ? await fetchNewsData(category, page || 1) : await fetchTrendingTopics(page || '');
+
     let filteredNews = newsList;
 
     // Simulate network delay
